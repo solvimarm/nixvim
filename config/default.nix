cc11730cb4440859776b2f54a7d1a9500495184b
solvimarm <solvimar@gmail.com>
2024-09-06 12:39:55 +0000

    harpoon

--- config/default.nix
+++ config/default.nix
@@ -13,5 +13,6 @@
     ./git.nix
     ./lualine.nix
     ./luasnip.nix
+    ./harpoon.nix
   ];
 }
---
+++ config/harpoon.nix
@@ -0,0 +1,16 @@
+{
+        # harpoon
+        plugins.harpoon = {
+            enable = true;
+            enableTelescope = true;
+            keymaps = {
+                addFile = "<leader>a";
+                toggleQuickMenu = "<leader>d";
+            };
+            menu = {
+                height = 20;
+                width = 100;
+            };
+
+        };
+}
