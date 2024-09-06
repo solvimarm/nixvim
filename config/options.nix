f06733df10e15b87aa81c1c744500a9508f7ba2f
solvimarm <solvimar@gmail.com>
2024-09-06 11:31:49 +0000

    options

---
+++ config/options.nix
@@ -0,0 +1,16 @@
+{
+ globals.mapleader = " ";
+        opts = {
+            number = true;
+            relativenumber = true;
+            shiftwidth = 4;
+        };
+        enable = true;
+        defaultEditor = true;
+        colorschemes.catppuccin = {
+            enable = true;
+            settings = {
+                transparent_background = true;
+            };
+        };
+}
