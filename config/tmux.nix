f07fc2ebb89383b1d6ed02f69d34de7e58521f32
solvimarm <solvimar@gmail.com>
2024-09-16 12:58:16 +0000

    tmux nav

--- config/tmux.nix
+++ config/tmux.nix
@@ -1,3 +1,22 @@
 {
     plugins.tmux-navigator.enable = true;
+    plugins.tmux-navigator.keymaps = [
+	{
+	    action = "left";
+	    key = "<C-w>h";
+	}
+	{
+	    action = "down";
+	    key = "<C-w>j";
+	}
+	{
+	    action = "up";
+	    key = "<C-w>k";
+	}
+	{
+	    action = "right";
+	    key = "<C-w>l";
+	  }
+    ];
+
 }
